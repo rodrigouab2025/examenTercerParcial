774c61e09380817d94918ff2cdca0d6dd752e63d
KhyabethMoyaPach <khyabeth.moya@uab.edu.bo>
2025-04-18 19:35:20 -0400

    Agrega los archivos del frontend del proyecto

---
+++ src/app/componentes/farmacia/farmacia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-farmacia',
+  imports: [],
+  templateUrl: './farmacia.component.html',
+  styleUrl: './farmacia.component.css'
+})
+export class FarmaciaComponent {
+  
+  
+}
